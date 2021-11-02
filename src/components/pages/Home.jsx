4afeb8877a3377dc7c7a60a4f56d0adf13e734b7
abeastaa <85842027+abeastaa@users.noreply.github.com>
2021-11-02 13:11:54 +0330

    Add all my files

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../../App.css";
+import HeroSection from "../common/HeroSection";
+import Cards from "../common/Cards";
+import Footer from "../common/Footer";
+
+function Home() {
+  return (
+    <>
+      <HeroSection />
+      <Cards />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
